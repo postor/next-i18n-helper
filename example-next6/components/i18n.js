f4feb0574505d63cc5ef0b8c0c318883e574a3bb
josh <postor@gmail.com>
2018-08-30 16:02:58 +0800

    next6.1.1的样例

---
+++ example-next6/components/i18n.js
@@ -0,0 +1,9 @@
+import I18nHelper from 'next-i18n-helper'
+import getWrapper from 'next-i18n-helper/dist/wrapper'
+
+export const i18nHelper = new I18nHelper({
+  defaultLang: 'zh',
+  supportLangs: ['en','zh']
+})
+
+export const wrapper = getWrapper(i18nHelper)
